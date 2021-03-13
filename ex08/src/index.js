edb2e5edc3af6921b2dcf1552e2f609aa7a3306d
tariktarik <tarik.bajric@gmail.com>
2021-03-13 10:50:38 +0100

    add

---
+++ ex08/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+    // Only change code below this line
+
+    var lastName = "Doe";
+    var lastNameLength;
+
+    lastNameLength = lastName.length;
+    // Only change code above this line
+    return lastNameLength;
+}
+
+console.log(main());
+module.exports = main;
---
+++ ex14/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+    // Only change code below this line
+
+    var myNoun = "dog";
+    var myVerb = "ran";
+    var myAdjective = "big";
+    var myAdverb = "quickly"; 
+    var wordBlanks = "My " + myNoun + " Rex " + myVerb + " away from a " + myAdjective + " dog so " + myAdverb + ".";
+    
+    // Only change code above this line
+    return wordBlanks;
+}
+
+console.log(main());
+module.exports = main;
